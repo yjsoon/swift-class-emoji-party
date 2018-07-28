aaf5010871f770e99bb92faac8457c3dc441c9af
YJ Soon <yjsoon@tinkertanker.com>
2018-07-28 09:52:45 +0800

    Added CollectionView protocol code

--- Emoji Party/EmojiViewController.swift
+++ Emoji Party/EmojiViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class EmojiViewController: UIViewController {
+private let reuseIdentifier = "Cell"
 
+class EmojiViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,17 @@ class EmojiViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
+
     }
 
 
