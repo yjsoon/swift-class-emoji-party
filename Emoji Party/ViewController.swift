b9633d93f15a42a851a78101a549c65547d2e0fc
YJ Soon <yjsoon@tinkertanker.com>
2018-07-28 09:29:40 +0800

    Initial Commit

---
+++ Emoji Party/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Party
+//
+//  Created by Soon Yin Jie on 28/7/18.
+//  Copyright © 2018 Tinkercademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
